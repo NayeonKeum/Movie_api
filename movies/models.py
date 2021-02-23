c6c496dd15d6ce4e5c7f3d742934d1d88c3f8c38
nkeum <rmaskdus0208@gmail.com>
2021-02-23 14:45:25 +0900

    add movies

---
+++ movies/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title=models.CharField(max_length=30)
+    genre=models.CharField(max_length=15)
+    year=models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True, help_text="post created date")
+    updated_at = models.DateTimeField(auto_now=True, help_text="post updated date")
+
+    def __str__(self):
+        return self.title
