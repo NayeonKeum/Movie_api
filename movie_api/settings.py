0b8e8a8407160d6ac069f624e57efbc2fbdc8c21
nkeum <rmaskdus0208@gmail.com>
2021-02-23 15:05:29 +0900

    erase secretkey

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b0k)6w_7#pc1mq@&+^2aqw6-4gm-9jihjizx3lxob6p7ep14jo'
+#SECRET_KEY = '.secretkey.txt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
